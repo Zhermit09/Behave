d7e584d7eab25dbccb45e5bf44ab362fc9de3e10
Pavel Urvantcev <pavel.urvantcev@gmail.com>
2024-10-02 10:16:22 +0200

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,60 @@
-package fuckAll;
-
 import java.io.*;
 import java.net.Socket;
 import java.net.URI;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
-public class Behave {
-
+public class Main {
     public static LocalTime from = LocalTime.of(22, 0);
     public static LocalTime to = LocalTime.of(6, 0);
     public static LocalTime fromW = LocalTime.of(0, 0);
     public static LocalTime toW = LocalTime.of(6, 0);
     static LocalDateTime time;
 
-
     public static void main(String[] args) {
 
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(() -> {
+        while (true) {
             load();
             try (Socket socket = new Socket("time.nist.gov", 13); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                 String responce;
                 while ((responce = in.readLine()) == null || responce.isEmpty()) ;
                 convertTime(responce.substring(6, 23));
-                throw new Exception();
 
             } catch (Exception e) {
                 time = LocalDateTime.now();
             }
 
-            DayOfWeek day = time.getDayOfWeek();
+            DayOfWeek day;
+            if (time.toLocalTime().isAfter(to.isAfter(toW) ? to : toW)) {
+                day = time.getDayOfWeek();
+            } else {
+                day = time.getDayOfWeek().minus(1);
+            }
+
             if (day == DayOfWeek.FRIDAY || day == DayOfWeek.SATURDAY) {
                 checkTime(fromW, toW);
             } else {
                 checkTime(from, to);
             }
+            try {
+                Thread.sleep(5 * 60 * 1000);
+            } catch (Exception ignore) {
+            }
+        }
 
-        }, 0, 5, TimeUnit.MINUTES);
     }
 
     public static void load() {
         try {
-        URI uri = new URI("https://raw.githubusercontent.com/Zhermit09/Behave/refs/heads/main/src/main/resources/timeFrame.txt");
+            URI uri = new URI("https://raw.githubusercontent.com/Zhermit09/Behave/refs/heads/main/src/main/resources/timeFrame.txt");
             BufferedReader timeFrames = new BufferedReader(new InputStreamReader(uri.toURL().openStream()));
             String line;
             while ((line = timeFrames.readLine()) != null) {
                 String[] args = Arrays.stream(line.split("-")).filter(e -> !e.isEmpty()).toArray(String[]::new);
                 for (String arg : args) {
                     String[] temp = arg.trim().split(" ");
-                    String[] time  = temp[1].trim().split(":");
+                    String[] time = temp[1].trim().split(":");
                     int h = Integer.parseInt(time[0]) % 24;
                     int m = Integer.parseInt(time[1]) % 60;
                     switch (temp[0].trim()) {
@@ -97,11 +98,9 @@ public class Behave {
 
     public static void kill() {
         try {
-            Runtime.getRuntime().exec(new String[]{"notepad.exe"}); //shutdown -s -f -t 0
+            Runtime.getRuntime().exec(new String[]{/*"shutdown", "-s", "-f", "-t", "0" /**/  "notepad.exe"/**/});
         } catch (Exception ignored) {
             kill();
         }
     }
-
 }
-
